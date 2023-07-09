e0e53e1dea27342f1119d912350c090f5d1d3f2a
HasnaHenaMow <hasna.itsd@goldenharvestbd.com>
2023-07-09 15:20:24 +0600

    11.10 & 11.11

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,21 @@
 import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
+import userRouter from './app/modules/users/user.route'
 const app: Application = express()
+
+import userService from './app/modules/users/user.service'
 
 app.use(cors())
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.get('/', (req: Request, res: Response) => {
+// Application Routes
+
+app.use('/api/v1/user/', userRouter)
+
+app.get('/', async (req: Request, res: Response) => {
+  await userService.createUser({ id: '999', password: '1234', role: 'student' })
   res.send('Working Successfully!')
 })
 
--- src/app/modules/users/user.service.ts
+++ src/app/modules/users/user.service.ts
@@ -1,9 +1,16 @@
+import config from '../../../config'
 import { IUser } from './user.interface'
 import { User } from './user.model'
+import { generateUserId } from './user.utils'
 
 const createUser = async (user: IUser): Promise<IUser | null> => {
   // Auto generated incremental ID
+  const id = await generateUserId()
+  user.id = id
   // Default Password
+  if (!user.password) {
+    user.password = config.default_user_pass as string
+  }
 
   const createdUser = await User.create(user)
   if (!createdUser) {
