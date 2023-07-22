656fc24a012466fbe95a32c2474968f0ae53be08
HasnaHenaMow <hasna.itsd@goldenharvestbd.com>
2023-07-22 17:20:14 +0600

    12-4 AUM-10

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,17 @@
 import mongoose from 'mongoose'
 import app from './app'
 import config from './config/index'
-
+import logger from './shared/logger'
 async function bootstrap() {
   try {
     await mongoose.connect(config.database_url as string)
-    console.log(`🛢  Database is connected successfullly!`)
+    logger.info(`🛢  Database is connected successfullly!`)
 
     app.listen(config.port, () => {
-      console.log(`App listening on port ${config.port}`)
+      logger.info(`App listening on port ${config.port}`)
     })
   } catch (err) {
-    console.log('Failed to connect database', err)
+    logger.error('Failed to connect database', err)
   }
 }
 
