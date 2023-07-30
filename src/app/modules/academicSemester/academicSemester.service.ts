a6167efcda4f849ee5d4d98185c28f8ce7be3d55
HasnaHenaMow <hasna.itsd@goldenharvestbd.com>
2023-07-30 15:36:22 +0600

    13-12 Semester Code Validation in Service

--- src/app/modules/academicSemester/academicSemester.service.ts
+++ src/app/modules/academicSemester/academicSemester.service.ts
@@ -1,12 +1,19 @@
+import ApiError from '../../../errors/ApiError'
+import { academicSemesterTitleCodeMap } from './academicSemester.constant'
 import { IAcademicSemester } from './academicSemester.interface'
 import { AcademicSemester } from './academicSemester.model'
+import httpStatus from 'http-status'
 
 const createSemester = async (
   payload: IAcademicSemester
 ): Promise<IAcademicSemester> => {
+  if (academicSemesterTitleCodeMap[payload.title] !== payload.code) {
+    throw new ApiError(httpStatus.BAD_REQUEST, 'Invalid Semester Code')
+  }
   const result = await AcademicSemester.create(payload)
   return result
 }
+
 export const AcademicSemesterService = {
   createSemester,
 }
